8bff235b10e9e89086a39e76f29e12aaa5b3ab66
Amio <amio.cn@gmail.com>
2014-06-15 09:32:46 +0800

    Remove three.js

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 window.onload = function () {
 
-  var camera, scene, renderer;
-  var geometry, material, mesh;
+  new FizzyText(document.getElementById('namesCanvas'), 'Design For Touch');
 
-  init();
-  animate();
-
-  function init() {
-
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 20000);
-    camera.position.z = 700;
-
-    scene = new THREE.Scene();
-
-    geometry = new THREE.BoxGeometry(200, 200, 200);
-    material = new THREE.MeshBasicMaterial({
-      color: 0xffee00,
-      wireframe: true
-    });
-
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    document.body.appendChild(renderer.domElement);
-
-  }
-
-  function animate() {
-
-    // note: three.js includes requestAnimationFrame shim
-    requestAnimationFrame(animate);
-
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.05;
-
-    renderer.render(scene, camera);
-
-  }
 };
+
+function FizzyText(wrapper, message) {
+
+  var wrapperRect = wrapper.getBoundingClientRect();
+  var width = wrapperRect.width;
+  var height = wrapperRect.height;
+  var textAscent = 101;
+  var textOffsetLeft = 80;
+  var noiseScale = 300;
+
+  var colors = ["#00aeff", "#0fa954", "#54396e", "#e61d5f"];
+
+  // This is the context we use to get a bitmap of text using
+  // the getImageData function.
+  var r = document.createElement('canvas');
+  var s = r.getContext('2d');
+
+  // This is the context we actually use to draw.
+  var c = document.createElement('canvas');
+  var g = c.getContext('2d');
+
+  r.setAttribute('width', width);
+  c.setAttribute('width', width);
+  r.setAttribute('height', height);
+  c.setAttribute('height', height);
+
+  // Add our demo to the HTML
+  wrapper.appendChild(c);
+
+}
